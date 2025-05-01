e3a163800a0fce1e3154474a4c8cfc66670ec05c
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2025-05-01 11:45:07 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 76 ms, Memory: 8844 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+bool compare(int a, int b)
+{
+	return a > b;
+}
+
+void bfs(queue<int> Q, vector<vector<int>>& E, int R, vector<bool>& C, vector<int>& A, int& count)
+{
+	C[R] = true;
+	A[R] = count;
+	count++;
+	Q.push(R);
+
+	while (!Q.empty())
+	{
+		int u = Q.front();
+		Q.pop();
+		for (int i = 0; i < E[u].size(); i++)
+		{
+			if (!C[E[u][i]])
+			{
+				C[E[u][i]] = true;
+				A[E[u][i]] = count;
+				count++;
+				Q.push(E[u][i]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int V, M, R;
+	cin >> V >> M >> R;
+	vector<vector<int>> E(V);
+	int start, end;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> start >> end;
+		E[start - 1].push_back(end - 1);
+		E[end - 1].push_back(start - 1);
+	}
+
+	for (int i = 0; i < V; i++)
+	{
+		sort(E[i].begin(), E[i].end(), compare);
+	}
+
+	vector<bool> C(V, false);
+	vector<int> A(V, 0);
+	int count = 1;
+	queue<int> Q;
+
+	bfs(Q, E, R - 1, C, A, count);
+
+	for (int i = 0; i < V; i++)
+	{
+		cout << A[i] << "\n";
+	}
+}
