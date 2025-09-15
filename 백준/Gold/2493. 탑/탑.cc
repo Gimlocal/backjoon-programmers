b500cfa747cbe62156f2b00c054b42933e6db183
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2025-09-15 15:46:31 +0900

    [Gold V] Title: 탑, Time: 284 ms, Memory: 10100 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -7,37 +7,35 @@ using namespace std;
 int main()
 {
 	int N; cin >> N;
-	stack<int> s;
-	map<int, int> idx;
+	stack<pair<int, int>> s;
 	vector<int> v(N);
 	vector<int> ans(N);
 	for (int i = 0; i < N; i++)
 	{
 		cin >> v[i];
-		idx[v[i]] = i + 1;
 	}
 
 	ans[0] = 0;
-	s.push(v[0]);
+	s.push({ v[0], 1 });
 
 	for (int i = 1; i < N; i++)
 	{
 		while (!s.empty())
 		{
-			if (s.top() < v[i])
+			if (s.top().first < v[i])
 			{
 				s.pop();
 				if (s.empty())
 				{
-					s.push(v[i]);
+					s.push({ v[i], i + 1 });
 					ans[i] = 0;
 					break;
 				}
 			}
 			else
 			{
-				ans[i] = idx[s.top()];
-				s.push(v[i]);
+				ans[i] = s.top().second;
+				s.push({ v[i], i + 1 });
 				break;
 			}
 		}
